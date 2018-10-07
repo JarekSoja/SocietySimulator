e432412990192a19552f5cb4bd4d1c0a446e1979
JarekSoja <jarek.soja@gmail.com>
2018-10-07 19:42:23 +0200

    Cleaning

--- src/main/java/com/soja/societysimulator/view/charts/BarChart.java
+++ src/main/java/com/soja/societysimulator/view/charts/BarChart.java
@@ -74,19 +74,16 @@ public class BarChart implements StreamResource.StreamSource {
 
     private static CategoryDataset createDataset() {
 
-        // row keys...
         String series1 = "First";
         String series2 = "Second";
         String series3 = "Third";
 
-        // column keys...
         String category1 = "Category 1";
         String category2 = "Category 2";
         String category3 = "Category 3";
         String category4 = "Category 4";
         String category5 = "Category 5";
 
-        // create the dataset...
         DefaultCategoryDataset dataset = new DefaultCategoryDataset();
 
         dataset.addValue(1.0, series1, category1);
@@ -113,9 +110,7 @@ public class BarChart implements StreamResource.StreamSource {
 
     private static JFreeChart createchart(CategoryDataset dataset) {
 
-        // create the chart...
         JFreeChart chart = ChartFactory.createBarChart("Bar Chart Demo 1", // chart
-                // title
                 "Category", // domain axis label
                 "Value", // range axis label
                 dataset, // data
@@ -125,35 +120,18 @@ public class BarChart implements StreamResource.StreamSource {
                 false // URLs?
         );
 
-        // NOW DO SOME OPTIONAL CUSTOMISATION OF THE CHART...
-
-        // set the background color for the chart...
         chart.setBackgroundPaint(Color.white);
 
-        // get a reference to the plot for further customisation...
         CategoryPlot plot = (CategoryPlot) chart.getPlot();
         plot.setBackgroundPaint(Color.lightGray);
         plot.setDomainGridlinePaint(Color.white);
         plot.setDomainGridlinesVisible(true);
         plot.setRangeGridlinePaint(Color.white);
 
-        // ******************************************************************
-        // More than 150 demo applications are included with the JFreeChart
-        // Developer Guide...for more information, see:
-        //
-        // > http://www.object-refinery.com/jfreechart/guide.html
-        //
-        // ******************************************************************
-
-        // set the range axis to display integers only...
         final NumberAxis rangeAxis = (NumberAxis) plot.getRangeAxis();
         rangeAxis.setStandardTickUnits(NumberAxis.createIntegerTickUnits());
 
-        // disable bar outlines...
         BarRenderer renderer = (BarRenderer) plot.getRenderer();
-        // renderer.setDrawBarOutline(false);
-
-        // set up gradient paints for series...
         GradientPaint gp0 = new GradientPaint(0.0f, 0.0f, Color.blue, 0.0f,
                 0.0f, new Color(0, 0, 64));
         GradientPaint gp1 = new GradientPaint(0.0f, 0.0f, Color.green, 0.0f,
@@ -167,7 +145,6 @@ public class BarChart implements StreamResource.StreamSource {
         CategoryAxis domainAxis = plot.getDomainAxis();
         domainAxis.setCategoryLabelPositions(CategoryLabelPositions
                 .createUpRotationLabelPositions(Math.PI / 6.0));
-        // OPTIONAL CUSTOMISATION COMPLETED.
 
         return chart;
 
@@ -223,11 +200,6 @@ public class BarChart implements StreamResource.StreamSource {
         series.add(6, 20);
         ds.addSeries(series);
 
-        // Paint p = new Color(0, 0, 0, Color.OPAQUE);
-        // r.setSeriesPaint(0, p);
-        // BasicStroke s = new BasicStroke(2);
-        // r.setSeriesStroke(0, s);
-
         DefaultTableXYDataset ds2 = new DefaultTableXYDataset();
         series = new XYSeries("DOO", false, false);
         series.add(1, 60);
@@ -273,8 +245,6 @@ public class BarChart implements StreamResource.StreamSource {
         XYPlot plot = (XYPlot) scatterPlot.getPlot();
 
         double[] regression = Regression.getOLSRegression(ds, 0);
-
-        // regression line points
 
         double v1 = regression[0] + regression[1] * 1;
         double v2 = regression[0] + regression[1] * 6;
